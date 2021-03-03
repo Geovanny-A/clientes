756067bfb6761af4818efa198f2e3957a794c80f
Geovanny Afanador <Geovany.afanador@gmail.com>
2021-03-03 00:36:34 -0500

    commit crud

---
+++ database/migrations/2021_03_03_040652_create_clientes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 45);
+            $table->string('tipo_documento', 45);
+            $table->string('documento');
+            $table->string('estado', 45);
+            $table->string('direccion', 45);
+            $table->string('barrio', 45);
+            $table->string('celular', 20);
+            $table->date('fecha_nacimiento');
+            $table->string('genero', 45);
+            $table->string('email', 45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
---
+++ resources/views/cliente/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.plantillaBase');
+
+@section('contenido')
+<h1>hola</h1>
+@endsection
